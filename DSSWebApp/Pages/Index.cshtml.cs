cf23e4eea8ba8d0866022a09cd5e912828f27692
Mohammed Gaber Ramadan <m.gaber2020@gmail.com>
2023-10-18 19:29:32 +0300

    3

--- DSSWebApp/Pages/Index.cshtml.cs
+++ DSSWebApp/Pages/Index.cshtml.cs
@@ -1,70 +1,65 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace DSSWebApp.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly IConfiguration _configuration;
 
-
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration;
         }
 
-        [BindProperty]
-        public string PQ1 { get; set; }
-        public string RecommendationHtml { get; set; }
+        public List<Question> Questions { get; set; }
 
         public void OnGet()
         {
-            // Project Characteristics Questions
-            ViewData["ProjectQuestion1"] = "How large is the project team?";
-            ViewData["ProjectQuestion2"] = "How complex is the project?";
-            ViewData["ProjectQuestion3"] = "How much uncertainty is there in the project?";
-            ViewData["ProjectQuestion4"] = "How tight are the project's deadlines?";
-            ViewData["ProjectQuestion5"] = "How constrained is the project's budget?";
-            ViewData["ProjectQuestion6"] = "How involved is the customer in the project?";
-            ViewData["ProjectQuestion7"] = "How high are the risk factors in the project?";
-            ViewData["ProjectQuestion8"] = "What is the type of software project?";
-
-            // Team Characteristics Questions
-            ViewData["TeamQuestion1"] = "What is the average level of experience of the project team members?";
-            ViewData["TeamQuestion2"] = "What is the average level of skills and expertise of the project team members?";
-            ViewData["TeamQuestion3"] = "How good is the communication and collaboration within the project team?";
-            ViewData["TeamQuestion4"] = "How high is the level of trust and commitment within the project team?";
-            ViewData["TeamQuestion5"] = "How diverse is the project team in terms of culture and background?";
-            ViewData["TeamQuestion6"] = "To what extent does your team have experience using agile software development methodologies?";
-            ViewData["TeamQuestion7"] = "To what extent does your team have experience using cloud-based software development tools?";
-            ViewData["TeamQuestion8"] = "How frequently do team members leave and join the team?";
-
-            // Options for Questions
-            ViewData["Option1"] = "1 = very low";
-            ViewData["Option2"] = "2 = low";
-            ViewData["Option3"] = "3 = medium";
-            ViewData["Option4"] = "4 = high";
-            ViewData["Option5"] = "5 = very high";
-
-            // Modify options for specific questions
-            ViewData["Option1PQ8"] = "1 = New Development";
-            ViewData["Option2PQ8"] = "2 = Maintenance & Enhancement";
-            ViewData["Option3PQ8"] = "3 = Integration";
-            ViewData["Option4PQ8"] = "4 = Migration";
-            ViewData["Option5PQ8"] = "5 = Evaluation";
-        }
-
-        public RedirectToPageResult OnPost()
+            try
             {
-            // Set the TempData property
-            TempData["PQ1"] = PQ1;
-            // ... Repeat for other questions ...
-
-            // Redirect to the Results page
-            return RedirectToPage("/Results");
-
+                var questionsJson = System.IO.File.ReadAllText("questions.json");
+                Questions = JsonConvert.DeserializeObject<List<Question>>(questionsJson);
+            }
+            catch (System.IO.FileNotFoundException ex)
+            {
+                _logger.LogError(ex, "questions.json file was not found.");
+                ModelState.AddModelError(string.Empty, "There was an error loading the questions. Please try again later.");
+            }
+            catch (JsonException ex)
+            {
+                _logger.LogError(ex, "Error deserializing questions.json.");
+                ModelState.AddModelError(string.Empty, "There was an error processing the questions. Please try again later.");
+            }
+            catch (System.Exception ex)
+            {
+                _logger.LogError(ex, "Unexpected error.");
+                ModelState.AddModelError(string.Empty, "An unexpected error occurred. Please try again later.");
+            }
         }
 
-       
+        public IActionResult OnPost()
+        {
+            var answers = new Dictionary<string, string>();
+            foreach (var key in Request.Form.Keys)
+            {
+                if (key != "__RequestVerificationToken") // Exclude the CSRF token
+                {
+                    answers.Add(key, Request.Form[key]);
+                }
+            }
+
+            // Logging the answers for debug purposes
+            _logger.LogInformation("Received answers: {@Answers}", answers);
+
+            TempData["answers"] = answers;
+            return RedirectToPage("/Results");
+        }
     }
 }
---
+++ DSSWebApp/Pages/Question.cs
@@ -0,0 +1,9 @@
+﻿namespace DSSWebApp.Pages
+{
+    public class Question
+    {
+        public string Id { get; set; }
+        public string Text { get; set; }
+        public List<string> Options { get; set; }
+    }
+}
