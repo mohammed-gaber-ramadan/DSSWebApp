cd6cda67d70b175865cff520b01f5dfb41e023d3
Mohammed Gaber Ramadan <m.gaber2020@gmail.com>
2023-10-19 15:49:16 +0300

    10

--- DSSWebApp/Pages/Results.cshtml.cs
+++ DSSWebApp/Pages/Results.cshtml.cs
@@ -1,19 +1,11 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.Routing;
-using System.ComponentModel.Design;
-using System.Diagnostics;
-using System.Reflection;
-using System.Reflection.Emit;
-using System.Runtime.InteropServices;
-using System.Text.RegularExpressions;
+using System.Collections.Generic;
 
 namespace DSSWebApp.Pages
 {
     public class ResultsModel : PageModel
     {
         public Dictionary<string, string> Answers { get; set; } = new Dictionary<string, string>();
-
 
         public bool IsTrustRecommendation { get; set; }
         public bool IsTurnoverRecommendation { get; set; }
@@ -26,149 +18,73 @@ namespace DSSWebApp.Pages
         public bool IsAgileRecommendation { get; set; }
         public bool IsWaterfallRecommendation { get; set; }
 
+        private string teamSizeAnswer = "", complexAnswer = "", budgetAnswer = "";
 
         public void OnGet()
         {
-            // Get the answers dictionary from the ViewData property.
-            Answers = TempData["answers"] as Dictionary<string, string>;
+            if (!TempData.TryGetValue("answers", out var answersData) || !(answersData is Dictionary<string, string> tempAnswers))
+            {
+                return;
+            }
 
-            var strProjectTeamSizeAnswer = "";
-            var strProjectComplexAnswer = "";
-            var strProjectBudgetAnswer = "";
+            Answers = tempAnswers;
 
             foreach (var answer in Answers)
             {
-                if (answer.Key == "Q12")
+                switch (answer.Key)
                 {
-                    //How high is the level of trust and commitment within the project team?
-                    if (answer.Value == "Very low" || answer.Value == "Low" || answer.Value == "Medium")
-                    {
-                        IsTrustRecommendation = true;
-                    }
-                    else
-                    {
-                        IsTrustRecommendation = false;
-                    }
-                }
-                if (answer.Key == "Q16")
-                {
-                    //How high is the level of trust and commitment within the project team?
-                    if (answer.Value == "Sometimes" || answer.Value == "Often" || answer.Value == "Very often")
-                    {
-                        IsTurnoverRecommendation = true;
-                    }
-                    else
-                    {
-                        IsTurnoverRecommendation = false;
-                    }
-                }
-                if (answer.Key == "Q10")
-                {
-                    // What is the average level of skills and expertise of the project team members?
-                    if (answer.Value == "Very low" || answer.Value == "Low" || answer.Value == "Medium")
-                    {
-                        IsSkillsRecommendation = true;
-                    }
-                    else
-                    {
-                        IsSkillsRecommendation = false;
-                    }
-                }
-                //How large is the project team?
-                if (answer.Key == "Q1")
-                {
-                    strProjectTeamSizeAnswer = answer.Value;
-                }
-                //How complex is the project?
-                if (answer.Key == "Q2")
-                {
-                    strProjectComplexAnswer = answer.Value;
-                }
-                //How constrained is the project's budget?
-                if (answer.Key == "Q5")
-                {
-                    strProjectBudgetAnswer = answer.Value;
-                }
-                //What is the type of software project?
-                if (answer.Key == "Q8")
-                {
-                    if (answer.Value == "Maintenance & enhancement" || answer.Value == "Evaluation")
-                    {
-                        IsProjectTypeKanbanRecommendation = true;
-                    }
-                    else
-                    {
-                        IsProjectTypeKanbanRecommendation = false;
-                    }
-                    if (answer.Value == "New development" || answer.Value == "Integration" || answer.Value == "Migration")
-                    {
-                        IsProjectTypeScrumRecommendation = true;
-                    }
-                    else
-                    {
-                        IsProjectTypeScrumRecommendation = false;
-                    }
-                }
-                //To what extent does your team have experience using cloud-based software development tools?
-                if (answer.Key == "Q15")
-                {
-                    if (answer.Value == "Very little experience" || answer.Value == "Some experience" || answer.Value == "Moderate experience")
-                    {
-                        IsCloudToolsRecommendation = true;
-                    }
-                    else
-                    {
-                        IsCloudToolsRecommendation = false;
-                    }
-                }
-                //How much uncertainty is there in the project?
-                if (answer.Key == "Q3")
-                {
-                    if (answer.Value == "Medium" || answer.Value == "High" || answer.Value == "Very high")
-                    {
-                        IsAgileRecommendation = true;
-                    }
-                    else
-                    {
-                        IsAgileRecommendation = false;
-                    }
-                    if (answer.Value == "Very low" || answer.Value == "Low")
-                    {
-                        IsWaterfallRecommendation = true;
-                    }
-                    else
-                    {
-                        IsWaterfallRecommendation = false;
-                    }
+                    case "Q12":
+                        IsTrustRecommendation = CheckValues(answer.Value, new[] { "Very low", "Low", "Medium" });
+                        break;
+                    case "Q16":
+                        IsTurnoverRecommendation = CheckValues(answer.Value, new[] { "Sometimes", "Often", "Very often" });
+                        break;
+                    case "Q10":
+                        IsSkillsRecommendation = CheckValues(answer.Value, new[] { "Very low", "Low", "Medium" });
+                        break;
+                    case "Q1":
+                    case "Q2":
+                    case "Q5":
+                        CheckProjectConditions(answer);
+                        break;
+                    case "Q8":
+                        IsProjectTypeKanbanRecommendation = CheckValues(answer.Value, new[] { "Maintenance & enhancement", "Evaluation" });
+                        IsProjectTypeScrumRecommendation = CheckValues(answer.Value, new[] { "New development", "Integration", "Migration" });
+                        break;
+                    case "Q15":
+                        IsCloudToolsRecommendation = CheckValues(answer.Value, new[] { "Very little experience", "Some experience", "Moderate experience" });
+                        break;
+                    case "Q3":
+                        IsAgileRecommendation = CheckValues(answer.Value, new[] { "Medium", "High", "Very high" });
+                        IsWaterfallRecommendation = CheckValues(answer.Value, new[] { "Very low", "Low" });
+                        break;
                 }
             }
-            if ((strProjectTeamSizeAnswer == "Medium" || strProjectTeamSizeAnswer == "Large" || strProjectTeamSizeAnswer == "Very large")
-                &&
-                (strProjectComplexAnswer == "Medium" || strProjectComplexAnswer == "Complex" || strProjectComplexAnswer == "Very complex")
-                && (strProjectBudgetAnswer == "Very loose" || strProjectBudgetAnswer == "Loose" || strProjectBudgetAnswer == "Medium"))
-            {
-                IsSupportBigTeamsRecommendation = true;
-            }
-            else
-            {
-                IsSupportBigTeamsRecommendation = false;
-            }
-            if ((strProjectTeamSizeAnswer == "Small" || strProjectTeamSizeAnswer == "Very small")
-                &&
-                (strProjectComplexAnswer == "Simple" || strProjectComplexAnswer == "Very simple")
-                && (strProjectBudgetAnswer == "Tight" || strProjectBudgetAnswer == "Very tight"))
-            {
-                IsSupporSmallTeamsRecommendation = true;
-            }
-            else
-            {
-                IsSupporSmallTeamsRecommendation = false;
-            }
+        }
+
+        private void CheckProjectConditions(KeyValuePair<string, string> answer)
+        {
+            if (answer.Key == "Q1") teamSizeAnswer = answer.Value;
+            if (answer.Key == "Q2") complexAnswer = answer.Value;
+            if (answer.Key == "Q5") budgetAnswer = answer.Value;
+
+            IsSupportBigTeamsRecommendation = CheckValues(teamSizeAnswer, new[] { "Medium", "Large", "Very large" }) &&
+                                              CheckValues(complexAnswer, new[] { "Medium", "Complex", "Very complex" }) &&
+                                              CheckValues(budgetAnswer, new[] { "Very loose", "Loose", "Medium" });
+
+            IsSupporSmallTeamsRecommendation = CheckValues(teamSizeAnswer, new[] { "Small", "Very small" }) &&
+                                               CheckValues(complexAnswer, new[] { "Simple", "Very simple" }) &&
+                                               CheckValues(budgetAnswer, new[] { "Tight", "Very tight" });
+        }
+
+        private static bool CheckValues(string value, string[] matches)
+        {
+            return Array.Exists(matches, match => match == value);
         }
 
         public void OnPost()
         {
-
+            // Logic for post if needed
         }
     }
 }
